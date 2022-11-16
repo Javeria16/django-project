4da6f389866b3274a10f5b9400c081eab50732f6
Javeria16 <hakeemjaveriya16@gmail.com>
2022-11-16 12:35:57 +0530

    update and delete modules

--- javeriya/sheets/urls.py
+++ javeriya/sheets/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('amresh/', views.indexhtml, name='indexhtml'),
     path('myfile/', views.myfile, name='myfile'),
     path('slicy/', views.slicy, name='slicy'),
+    path('add/', views.add, name='add'),
+    path('add/addrecord/', views.addrecord, name='addrecord'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('update/<int:id>', views.update, name='update'),
+    path('update/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),
 
 ]
--- javeriya/sheets/views.py
+++ javeriya/sheets/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
 from django.template import loader
 from .models import Sheets
 
@@ -31,3 +32,46 @@ def slicy(request):
     'var': mymem,
   }
   return HttpResponse(template.render(context, request))
+
+
+def add(request):
+  template = loader.get_template('add.html')
+  context = {
+  
+  }
+  return HttpResponse(template.render(context, request))
+
+
+
+
+def addrecord(request):
+  x = request.POST['first']
+  y = request.POST['last']
+  member = Sheets(firstname=x, lastname=y)
+  member.save()
+  return HttpResponseRedirect(reverse('slicy'))
+  
+
+def delete(request ,id):
+  member = Sheets.objects.get(id=id)
+  member.delete()
+  return HttpResponseRedirect(reverse('slicy'))
+
+
+def update(request, id):
+  mymember = Sheets.objects.get(id=id)
+  template = loader.get_template('update.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+
+def updaterecord(request, id):
+  first = request.POST['first']
+  last = request.POST['last']
+  member = Sheets.objects.get(id=id)
+  member.firstname = first
+  member.lastname = last
+  member.save()
+  return HttpResponseRedirect(reverse('slicy'))
